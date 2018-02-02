62a797b0c21175e8c634f31ad28e519288470d89
carry0987 <carry0987@users.noreply.github.com>
2018-02-02 13:30:45 +0800

    Edit Js

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,6 @@
  var feedback = function(res) {
      if (res.success === true) {
-        var wahaha = res.data.link.replace("http", "https");
+        var wahaha = res.data.link.replace("http", "http");
         document.querySelector('.status').classList.add('bg-success');
         document.querySelector('.status').innerHTML = 
         'Image : ' + '<br><input class="image-url" value=' + wahaha + '/>' + '<img class="img" src=' + wahaha + '/>';
